e0b4edfe5f2498243aa6c92789d68153cbafe649
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-11-12 20:51:01 +0100

    attempt fix of OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CXXFLAGS="-O2 -Wno-deprecated"
-
-# mkdir -vp ${PREFIX}/bin;
 mkdir build; cd build;
 cmake $SRC_DIR \
 	  -DUSE_FFMPEG=ON \
@@ -12,6 +9,8 @@ cmake $SRC_DIR \
 	  -DUSE_OPENCV=OFF \
 	  -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_SYSTEM_IGNORE_PATH=/usr/lib \
-	  -DCMAKE_INSTALL_LIBDIR=lib
+	  -DCMAKE_INSTALL_LIBDIR=lib \
+	  -DCMAKE_BUILD_TYPE=Release \
+	  -DCMAKE_CXX_FLAGS="-DGIFLIB_MAJOR=5 -Wno-deprecated" # Hack for OS X build
 
-make install -j4
+make install -j${CPU_COUNT}
