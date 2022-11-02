32c334a82bbd4b9ca54d26c4b0ef281f5a113ff9
Valerio Viperino <v_viperino@apple.com>
2022-11-02 10:41:16 +0000

    debug: Apply Daniel's patch
    - Verbose cmake, for debugging
    - Specify python as requirement
    - Hardcode python paths (where to search for libs/headers), for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ export CXXFLAGS="$CXXFLAGS -Wno-deprecated -DGIFLIB_MAJOR=5"
 
 mkdir -vp ${PREFIX}/bin;
 mkdir build; cd build;
-cmake $SRC_DIR \
+cmake ${CMAKE_ARGS} $SRC_DIR \
+	  -DVERBOSE=1 \
 	  -DUSE_FFMPEG=ON \
 	  -DOIIO_BUILD_TOOLS=OFF \
 	  -DOIIO_BUILD_TESTS=OFF \
@@ -14,5 +15,7 @@ cmake $SRC_DIR \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DPYTHON_VERSION=$PY_VER \
+	  -DPYTHON_INCLUDE_DIR=$BUILD_PREFIX/include/python3.10 \
+	  -DPYTHON_LIBRARIES=$PREFIX/lib/libpython3.10.dylib
 
 make all -j${CPU_COUNT}
