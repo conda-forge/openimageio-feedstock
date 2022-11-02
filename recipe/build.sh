d7f570837ccc2ee9080f56d8fb4954c4317fbe2f
Valerio Viperino <v_viperino@apple.com>
2022-11-02 18:54:38 +0000

    Suppress several warnings for gcc/clang
    And force CXXFLAGS by passing it via DCMAKE_CXX_FLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-export CXXFLAGS="$CXXFLAGS -Wno-deprecated -Wno-unused-variable -Wunused-but-set-variable -DGIFLIB_MAJOR=5"
+# suppress warnings for clang..
+if [ $(uname) == "Darwin" ]; then
+  additional_cxx_flags='-Wno-unused-private-field'
+# and gcc
+elif [ $(uname) == "Linux" ]; then
+  additional_cxx_flags='-Wno-deprecated -Wno-unused-variable -Wno-unused-but-set-variable'
+else
+  additional_cxx_flags=''
+fi
+
+export CXXFLAGS="$CXXFLAGS $additional_cxx_flags -DGIFLIB_MAJOR=5"
+
 PY_MAJ_MIN_VER=$(echo $PY_VER | awk -F '.' '{print $1"."$2}')
 
 mkdir -vp ${PREFIX}/bin;
@@ -19,6 +30,7 @@ cmake ${CMAKE_ARGS} $SRC_DIR \
 	  -DPYTHON_INCLUDE_DIR=$BUILD_PREFIX/include/python$PY_MAJ_MIN_VER \
 	  -DPYTHON_LIBRARIES=$PREFIX/lib/libpython${PY_MAJ_MIN_VER}.dylib \
 	  -DUSE_STD_REGEX_EXITCODE=0 \
-	  -DUSE_STD_REGEX_EXITCODE__TRYRUN_OUTPUT=''
+	  -DUSE_STD_REGEX_EXITCODE__TRYRUN_OUTPUT='' \
+	  -DCMAKE_CXX_FLAGS=$CXXFLAGS
 
 make all -j${CPU_COUNT}
