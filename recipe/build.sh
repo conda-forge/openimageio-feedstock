02f2eaadd5b6d4dd75aa2a81843d5d7285a9807f
Mark Harfouche <mark.harfouche@gmail.com>
2023-10-02 09:26:17 -0400

    Update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,9 @@ mkdir -p ${PREFIX}/bin
 mkdir build
 
 pushd build;
+
+PYTHON_MODULE_EXTENSION=$(python -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
+
 cmake ${CMAKE_ARGS} \
     -DUSE_FFMPEG=ON \
     -DOIIO_BUILD_TOOLS=OFF \
@@ -17,6 +20,9 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=Release \
     -DPYTHON_VERSION=$PY_VER \
+    -DPYTHON_MODULE_EXTENSION=${PYTHON_MODULE_EXTENSION} \
+    -DPython_FIND_STRATEGY=LOCATION \
+    -DPython_FIND_VIRTUALENV=First \
     ..
 
 make all -j${CPU_COUNT}
