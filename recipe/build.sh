a5e1c02ae1a12c5b593457c7a562a97deb60ec08
Mark Harfouche <mark.harfouche@gmail.com>
2023-10-02 05:16:52 -0400

    specify PYTHON_MODULE_EXTENSION

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ mkdir -p ${PREFIX}/bin
 mkdir build
 
 pushd build;
+PYTHON_MODULE_EXTENSION=$(python -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
+
 cmake ${CMAKE_ARGS} \
     -DUSE_FFMPEG=ON \
     -DOIIO_BUILD_TOOLS=OFF \
@@ -17,6 +19,7 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=Release \
     -DPYTHON_VERSION=$PY_VER \
+    -DPYTHON_MODULE_EXTENSION=${PYTHON_MODULE_EXTENSION} \
     ..
 
 make all -j${CPU_COUNT}
