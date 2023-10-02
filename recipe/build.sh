4b96842b45c87409adc23f718926c22fd59f5c22
Mark Harfouche <mark.harfouche@gmail.com>
2023-10-01 20:29:22 -0400

    Specify CMAKE_ARGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,17 +2,23 @@
 
 export CXXFLAGS="$CXXFLAGS -DGIFLIB_MAJOR=5"
 
-mkdir -vp ${PREFIX}/bin;
-mkdir build; cd build;
-cmake $SRC_DIR \
-	  -DUSE_FFMPEG=ON \
-	  -DOIIO_BUILD_TOOLS=OFF \
-	  -DOIIO_BUILD_TESTS=OFF \
-	  -DUSE_PYTHON=ON \
-	  -DUSE_OPENCV=OFF \
-	  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-	  -DCMAKE_INSTALL_LIBDIR=lib \
-	  -DCMAKE_BUILD_TYPE=Release \
-	  -DPYTHON_VERSION=$PY_VER \
+mkdir -p ${PREFIX}/bin
+
+mkdir build
+
+pushd build;
+cmake ${CMAKE_ARGS} \
+    -DUSE_FFMPEG=ON \
+    -DOIIO_BUILD_TOOLS=OFF \
+    -DOIIO_BUILD_TESTS=OFF \
+    -DUSE_PYTHON=ON \
+    -DUSE_OPENCV=OFF \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPYTHON_VERSION=$PY_VER \
+    ..
 
 make all -j${CPU_COUNT}
+
+popd
