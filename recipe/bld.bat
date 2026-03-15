2c8e3f0d86ed79d3540fe013b87569251cc7e1e8
Tobias Fischer <info@tobiasfischer.info>
2026-03-16 07:55:24 +1000

    Add build script for CMake with Ninja generator

---
+++ recipe/bld.bat
@@ -0,0 +1,25 @@
+@echo on
+
+set CXXFLAGS=%CXXFLAGS% -DGIFLIB_MAJOR=5
+
+if not exist build mkdir build
+cd build
+
+cmake -G "Ninja" ^
+    %CMAKE_ARGS% ^
+    -DCMAKE_CXX_FLAGS="%CXXFLAGS%" ^
+    -DUSE_FFMPEG=ON ^
+    -DOIIO_BUILD_TOOLS=OFF ^
+    -DOIIO_BUILD_TESTS=OFF ^
+    -DUSE_PYTHON=ON ^
+    -DUSE_OPENCV=OFF ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DPYTHON_VERSION=%PY_VER% ^
+    -DPython_EXECUTABLE=%PYTHON% ^
+    -DBUILD_MISSING_FMT=OFF ^
+    -DINTERNALIZE_FMT=OFF ^
+    ..
+if errorlevel 1 exit 1
+
+cmake --build . --config Release
+if errorlevel 1 exit 1
